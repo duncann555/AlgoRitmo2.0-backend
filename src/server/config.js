e327d1ea3621b7c8d9b12947737d119d9b4edda2
Seba Flomenbaun <sebaflomen@gmail.com>
2025-11-23 22:33:35 -0300

    modifique archivo config.js, middleware cors

--- src/server/config.js
+++ src/server/config.js
@@ -13,15 +13,7 @@ export default class Server {
   }
 
   middlewares() {
-    this.app.use(
-      cors({
-        origin: "*",
-        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-        allowedHeaders: ["Content-Type", "Authorization"],
-      })
-    );
-    this.app.options("*", cors());
-
+    this.app.use(cors());
     this.app.use(express.json());
     this.app.use(morgan("dev"));
 
